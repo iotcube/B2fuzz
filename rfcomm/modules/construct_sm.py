fb320578bddc0da9a505ab8266c48bba33aae841
baehunsang <hoonsang99@korea.ac.kr>
2025-01-24 18:00:41 +0900

    construct_sm

--- rfcomm/modules/construct_sm.py
+++ rfcomm/modules/construct_sm.py
@@ -3,6 +3,22 @@ from .pairing import *
 from collections import defaultdict
 import copy
 import os
+from transitions.extensions import GraphMachine
+
+class Visualize:
+    def __init__(self) -> None:
+        self.m = GraphMachine(model=self, graph_engine="pygraphviz", 
+            states=["closed_state"],
+            initial= "closed_state"
+        )
+
+    def add_state(self, state):
+        self.m.add_state(state)
+
+    def add_tr(self, src, dst, frame):
+        self.m.add_transition(frame, src, dst)
+
+vis = Visualize()
 
 hidden_state_path = []
 
@@ -42,36 +58,66 @@ def construct_sm(target_addr, channel):
         # can open ctrl channel
         sock = opened_ctrl_ch(target_addr)
         if sock:
+
+            vis.add_state(state2str(OPENED_CTRL_CH))
+            vis.add_tr(state2str(CLOSED), state2str(OPENED_CTRL_CH), frame2str(SABM))
+            vis.add_tr(state2str(OPENED_CTRL_CH), state2str(CLOSED), frame2str(DISC))
+
             ret[CTRL_CHANNEL][OPENED_CTRL_CH] = [DISC, PN]
             sock.close()
 
         sock = closed_normal_ch(target_addr, channel)
         if sock:
+            vis.add_state(state2str(CLOSED_NORMAL_CH))
+            vis.add_tr(state2str(OPENED_CTRL_CH), state2str(CLOSED_NORMAL_CH), frame2str(PN))
+
             ret[channel][CLOSED_NORMAL_CH] = [SABM]
             if establish_dlci(sock, channel):
+
+                vis.add_state(state2str(OPENED_NORMAL_CH))
+                vis.add_tr( state2str(CLOSED_NORMAL_CH),state2str(OPENED_NORMAL_CH), frame2str(SABM))
+                vis.add_tr(state2str(OPENED_NORMAL_CH), state2str(CLOSED_NORMAL_CH), frame2str(DISC))
+
                 ret[channel][OPENED_NORMAL_CH] = [DISC, MSC]
             sock.close()
 
 
         sock, _ = open_normal_ch_with_msc(target_addr, channel)
         if sock:
+
+            vis.add_state(state2str(OPENED_NORMAL_CH_WITH_MSC))
+            vis.add_tr(state2str(OPENED_NORMAL_CH_WITH_MSC),state2str(CLOSED_NORMAL_CH), frame2str(DISC))
+            vis.add_tr(state2str(OPENED_NORMAL_CH), state2str(OPENED_NORMAL_CH_WITH_MSC), frame2str(MSC))
+            vis.add_tr(state2str(OPENED_NORMAL_CH_WITH_MSC), state2str(OPENED_NORMAL_CH_WITH_MSC), frame2str(DATA))
+
             ret[channel][OPENED_NORMAL_CH_WITH_MSC] = [DATA, DISC]
             sock.close()
 
         sock, new_dlci = open_new_chan(target_addr, channel)
         if sock:
             # ADD new channel SM
+            vis.add_state(state2str(CLOSED_NORMAL_CH)+str(new_dlci>>1))
+            vis.add_tr(state2str(OPENED_CTRL_CH), state2str(CLOSED_NORMAL_CH)+str(new_dlci>>1), frame2str(PN))
+
             ret[new_dlci>>1][CLOSED_NORMAL_CH] = [SABM]
             sock = establish_new_dlci(sock, new_dlci)
             if sock:
+                vis.add_state(state2str(OPENED_NORMAL_CH)+str(new_dlci>>1))
+                vis.add_tr(state2str(CLOSED_NORMAL_CH)+str(new_dlci>>1),state2str(OPENED_NORMAL_CH)+str(new_dlci>>1), frame2str(SABM))
+                vis.add_tr(state2str(OPENED_NORMAL_CH)+str(new_dlci>>1), state2str(CLOSED_NORMAL_CH)+str(new_dlci>>1), frame2str(DISC))
                 ret[new_dlci>>1][OPENED_NORMAL_CH] = [DISC, MSC]
             sock, msc = new_chan_msc(sock, new_dlci>>1, new_dlci&0b1)
             if msc:
+                vis.add_state(state2str(OPENED_NORMAL_CH_WITH_MSC)+str(new_dlci>>1))
+                vis.add_tr(state2str(OPENED_NORMAL_CH_WITH_MSC)+str(new_dlci>>1),state2str(CLOSED_NORMAL_CH)+str(new_dlci>>1), frame2str(DISC))
+                vis.add_tr(state2str(OPENED_NORMAL_CH)+str(new_dlci>>1), state2str(OPENED_NORMAL_CH_WITH_MSC)+str(new_dlci>>1), frame2str(MSC))
+                vis.add_tr(state2str(OPENED_NORMAL_CH_WITH_MSC)+str(new_dlci>>1), state2str(OPENED_NORMAL_CH_WITH_MSC)+str(new_dlci>>1), frame2str(DATA))
                 ret[new_dlci>>1][OPENED_NORMAL_CH_WITH_MSC] = [DATA, DISC]
             sock.close()
     except:
         pass
-    pprint(print_sm(ret))    
+    pprint(print_sm(ret))
+    vis.get_graph().draw("base_sm.png", prog='dot')
     return ret
 
 
@@ -110,7 +156,10 @@ def send_frame(sock, frame, ch, state, channel_to_ctrl, base_sm, ret_sm, path):
             ret_sm[ch][state].append(frame)
             ret_sm[ch][new_state] = []
             hidden_state_path.append([(path, frame)])
+            vis.add_state(state2str(new_state))
+            vis.add_tr(state2str(state),state2str(new_state), frame2str(frame))
             new_state += 1
+
     except:
         sock.close()
         return False
@@ -192,6 +241,8 @@ def expand_sm(sm, initial_channel, target_addr):
     except:
         pprint(print_sm(ret))
         print(hidden_state_path)
+        vis.get_graph().draw("expanded_sm.png", prog='dot')
         return False
     pprint(print_sm(ret))
     print(hidden_state_path)
+    vis.get_graph().draw("expanded_sm.png", prog='dot')
